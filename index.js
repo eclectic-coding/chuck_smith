8b846ca6e221a7884e67388fed1743a5b74ac723
Chuck Smith <chuck@eclecticsaddlebag.com>
2020-10-28 13:48:11 -0400

    First display setup

---
+++ index.js
@@ -0,0 +1,10 @@
+#!/usr/bin/env node
+console.log(`
+Chuck Smith
+
+Add bio
+
+🐦 Twitter: https://twitter.com/EclecticCoding 
+📖 GitHub: https://github.com/eclectic-coding
+😎 Blog: https://eclecticsaddlebag.com
+`);
