78021524be51d25dadc383f9b9b7ba847c567e61
eclecticcoding <chuck@eclecticsaddlebag.com>
2021-09-24 14:08:27 -0400

    Configure CLI header

--- index.js
+++ index.js
@@ -1,8 +1,18 @@
 #!/usr/bin/env node
-const clearConsole = require('clear-any-console');
+const pkgJSON = require('./package.json')
+const welcome = require('cli-welcome')
 
-// clear console
-clearConsole();
+// Welcome Header
+welcome({
+  title: pkgJSON.name,
+  tagLine: `by ${pkgJSON.author.name}`,
+  description: pkgJSON.description,
+  version: pkgJSON.version,
+  bgColor: `#FADC00`,
+  color: `#000000`,
+  bold: true,
+  clear: true
+});
 
 console.log(`
 Chuck Smith
